a520169187b6d83284a726ba6b893cc462fc1f31
= <=>
2025-04-26 03:04:31 +0530

    my commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import   Sidebar from './Components/Sidebar/Sidebar.jsx'
+import './Components/Sidebar/Sidebar.css';
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
