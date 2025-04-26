adb61d9c4686730fdb264f655deeffe40323d7cc
= <=>
2025-04-27 02:02:27 +0530

    updated Main.css and sidebar.jsx with new style and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import   Sidebar from './Components/Sidebar/Sidebar.jsx'
 import './Components/Sidebar/Sidebar.css';
+import Main from './Components/Main/Main.jsx';
 
 const App = () => {
   return (
     <>
       <Sidebar/>
+      <Main/>
     </>
   )
 }
